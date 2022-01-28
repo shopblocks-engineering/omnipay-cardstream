e3463223fc3788051a60e24819e7fb13a6eb7c5c
Jamie Peck <jamie.peck@live.com>
2022-01-28 09:25:16 +0000

    Revert "Cardstream Refunds Functionality"
    This reverts commit dc6c1c30d4e74308e3196f248900935aed0d6f15.

--- src/Message/RefundRequest.php
+++ src/Message/RefundRequest.php
@@ -12,12 +12,16 @@ class RefundRequest extends BaseRequest
         $data['merchantID'] = $this->getMerchantId();
         $data['action'] = $this->getAction(); // PREAUTH, VERIFY, SALE, REFUND, REFUND_SALE
         $data['type'] = 1; // ecommerce type
+        $data['formResponsive'] = 'Y';
         $data['xref'] = $this->getXref();
-
         // a delay of zero allows duplicate transactions to go through
         $data['duplicateDelay'] = $this->getTestMode() ? 0 : 300;
 
         $data['amount'] = $this->getAmountInteger();
+
+        if ($returnUrl = $this->getReturnUrl()) {
+            $data['redirectURL'] = $returnUrl;
+        }
 
         // Remove items we don't want to send in the request
         // (they may be there if a previous response is sent)
@@ -32,28 +36,77 @@ class RefundRequest extends BaseRequest
         ]);
         $data['signature'] = $this->getSigningString($data, $this->getMerchantSecret(), true);
 
-        return $this->prepare($data);
-    }
+        $this->validateParams([
+            'merchantID',
+            'merchantSecret',
+            'action',
+            'currencyCode',
+            'countryCode',
+        ], array_merge($data, ['merchantSecret' => $this->getMerchantSecret()]));
 
-    public function getEndpoint()
-    {
-        return static::$directUrl;
+        return $this->prepare($data);
     }
 
     public function sendData($data)
     {
-        $response = $this->httpClient->request(
-            'POST',
-            $this->getEndpoint(),
-            [
-                'Content-Type' => ' application/x-www-form-urlencoded',
-            ],
-            http_build_query($data)
-        );
-        
-        parse_str($response->getBody()->getContents(), $payload);
-
-        return new RefundResponse($this, $payload);
+        // TODO: Implement sendData() method.
     }
 
+    public function refundRequest()
+    {
+        $queryPayload = [
+            'merchantID' => $this->merchantID,
+            'xref' => $xref,
+            'action' => 'QUERY',
+        ];
+
+        $queryPayload['signature'] = static::sign($queryPayload, $this->merchantSecret);
+
+        $paymentInfo = $this->client->post($queryPayload);
+
+        $state = $paymentInfo['state'] ?? null;
+
+        $payload = [
+            'merchantID' => $this->merchantID,
+            'xref' => $xref,
+        ];
+
+        switch ($state) {
+            case 'approved':
+            case 'captured':
+                $payload['action'] = 'CANCEL';
+                break;
+            case 'accepted':
+                $payload = array_merge($payload, [
+                    'type' => 1,
+                    'action' => 'REFUND_SALE',
+                    'amount' => $amount,
+                ]);
+                break;
+            default:
+                throw new \InvalidArgumentException('Something went wrong, we can\'t find transaction '. $xref);
+        }
+
+        $payload['signature'] = static::sign($payload, $this->merchantSecret);
+        $res = $this->client->post($payload);
+
+        if (isset($res['responseCode']) && $res['responseCode'] == "0") {
+            $orderMessage = ($res['responseCode'] == "0" ? "Refund Successful" : "Refund Unsuccessful") . "<br/><br/>";
+
+            $state = $res['state'] ?? null;
+
+            if ($state != 'canceled') {
+                $orderMessage .= "Amount Refunded: " . (isset($res['amountReceived']) ? number_format($res['amountReceived'] / pow(10, $res['currencyExponent']), $res['currencyExponent']) : "None") . "<br/><br/>";
+            }
+
+            $orderMessage .=
+                "Message: " . $res['responseMessage'] . "<br/>" .
+                "xref: " . $res['xref'] . "<br/>";
+
+            return [
+                'message' => $orderMessage,
+                'response' => $res,
+            ];
+        }
+    }
 }
